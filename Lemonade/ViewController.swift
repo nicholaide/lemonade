71e0c8953bbed96228dc74e3a3591a6842e9e20f
Nicholai de Guzman <nicholaideguzman@nis-imac.dhcp.swmed.org>
2015-01-09 11:55:19 -0600

    connected labels and events

--- Lemonade/ViewController.swift
+++ Lemonade/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //You have
+    @IBOutlet weak var labelMoney: UILabel!
+    @IBOutlet weak var labelLemonInventory: UILabel!
+    @IBOutlet weak var labelCubeInventory: UILabel!
+    
+ 
+    //Purchase
+    @IBOutlet weak var labelLemonBuy: UILabel!
+    @IBOutlet weak var labelCubeBuy: UILabel!
+    
+    //Mix
+    @IBOutlet weak var labelLemonMix: UILabel!
+    @IBOutlet weak var labelCubeMix: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +34,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //adding
+    @IBAction func buttonLemonBuyAdd(sender: UIButton) {
+    }
 
+    @IBAction func buttonCubeBuyAdd(sender: UIButton) {
+    }
+    
+    @IBAction func buttonLemonMixAdd(sender: UIButton) {
+    }
+    
+    @IBAction func buttonCubeMixAdd(sender: UIButton) {
+    }
+    
+    //removing or subtracting
+    @IBAction func buttonLemonBuySub(sender: UIButton) {
+    }
+    
+    @IBAction func buttonCubeBuySub(sender: UIButton) {
+    }
+    
+    @IBAction func buttonLemonMixSub(sender: UIButton) {
+    }
+    
+    @IBAction func buttonCubeMixSub(sender: UIButton) {
+    }
+    
+    //main button
+    
+    @IBAction func buttonStart(sender: UIButton) {
+    }
 
 }
 
