1a09bf21ec38943acbc9e829362b2b06478a2039
Nicholai de Guzman <nicholaideguzman@nis-imac.dhcp.swmed.org>
2015-01-09 15:19:02 -0600

    adding, subtracting actions and label updates

--- Lemonade/ViewController.swift
+++ Lemonade/ViewController.swift
@@ -9,8 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //constants
+    let lemonPrice = 2
+    let cubePrice = 1
+    
+    //variables for computation
+    var lemonSupply = 1
+    var cubeSupply = 1
+    var cashSupply = 10
+    
+    var lemonBuy = 0
+    var cubeBuy = 0
+    var lemonMix = 0
+    var cubeMix = 0
 
-    //You have
+    //You have (supplies)
     @IBOutlet weak var labelMoney: UILabel!
     @IBOutlet weak var labelLemonInventory: UILabel!
     @IBOutlet weak var labelCubeInventory: UILabel!
@@ -37,33 +51,114 @@ class ViewController: UIViewController {
     
     //adding
     @IBAction func buttonLemonBuyAdd(sender: UIButton) {
+        if cashSupply - lemonPrice >= 0 {
+            cashSupply -= lemonPrice
+            lemonSupply++
+            lemonBuy++
+            
+            updateAllLabels()
+            
+        }
+        
     }
 
     @IBAction func buttonCubeBuyAdd(sender: UIButton) {
+        if cashSupply - cubePrice >= 0 {
+            cashSupply -= cubePrice
+            cubeSupply++
+            cubeBuy++
+
+            updateAllLabels()
+
+        }
+
     }
     
     @IBAction func buttonLemonMixAdd(sender: UIButton) {
+        if lemonSupply - 1 >= 0 {
+            lemonSupply--
+            lemonMix++
+         
+            updateAllLabels()
+        }
+
     }
     
     @IBAction func buttonCubeMixAdd(sender: UIButton) {
+        if cubeSupply - 1 >= 0 {
+            cubeSupply--
+            cubeMix++
+            updateAllLabels()
+        }
+    
     }
     
-    //removing or subtracting
+    //removing or subtracting functions 
+    
     @IBAction func buttonLemonBuySub(sender: UIButton) {
+        //need at least one lemon I think!
+        if lemonSupply - 1 >= 1 {
+            lemonSupply--
+            cashSupply += lemonPrice
+            lemonBuy--
+            
+            updateAllLabels()
+        }
+       
     }
     
     @IBAction func buttonCubeBuySub(sender: UIButton) {
+        //need at least one lemon I think!
+        if cubeSupply - 1 >= 1 {
+            cubeSupply--
+            cashSupply += cubePrice
+            cubeBuy--
+            
+            updateAllLabels()
+
+        }
+       
+        
     }
     
     @IBAction func buttonLemonMixSub(sender: UIButton) {
+        if lemonMix - 1 >= 0 {
+            lemonSupply++
+            lemonMix--
+            
+            updateAllLabels()
+        }
     }
     
     @IBAction func buttonCubeMixSub(sender: UIButton) {
+        if cubeMix - 1 >= 0 {
+            cubeSupply++
+            cubeMix--
+            
+            updateAllLabels()
+        }
     }
     
     //main button
-    
     @IBAction func buttonStart(sender: UIButton) {
+    }
+    
+    func updateAllLabels() {
+        //lemon
+        labelLemonInventory.text = "\(lemonSupply)" + " Lemons"
+        labelLemonBuy.text = "\(lemonBuy)"
+        labelLemonMix.text = "\(lemonMix)"
+        
+        
+        //cubes
+        labelCubeInventory.text = "\(cubeSupply)" + " Ice Cubes"
+        labelCubeBuy.text = "\(cubeBuy)"
+        labelCubeMix.text = "\(cubeMix)"
+        
+        
+        //cash
+        labelMoney.text = "$" + "\(cashSupply)"
+        
     }
 
 }
